cf7931ad808d8e5348f1a3fc394bc9975cfa4159
Zumi Yu <88576446+Yumi2121@users.noreply.github.com>
2023-06-01 16:19:11 +1000

    Create StudentRegistrationServlet.java

---
+++ src/com.softra.controller/StudentRegistrationServlet.java
@@ -0,0 +1,44 @@
+package com.softra.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StudentRegistrationServlet
+ */
+public class StudentRegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StudentRegistrationServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String age = request.getParameter("age");
+		String mobile = request.getParameter("mobile");
+		String address = request.getParameter("address");
+		
+		RequestDispatcher rd= request.getRequestDispatcher("/success.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
